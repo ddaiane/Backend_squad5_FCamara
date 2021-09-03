8bc68785b2faf89e3a00bec4b79eeb0fb747e497
Daiane <daianemarcon@gmail.com>
2021-09-03 02:19:43 -0300

    comenta codigo

--- js/module/requests.js
+++ js/module/requests.js
@@ -1,4 +1,7 @@
 var xmlHttp
+//o massa de modular o js é que algumas variaveis tipo essa de requisição http
+//podem ficar globais mas ficam acessivel só as funções aqui desse modulo ja que nao foi exportada!
+
 export var json
 
 export function fnRead() {
@@ -18,6 +21,9 @@ export function invertejson() {
     json = [json[randomNum(0,3)], json[randomNum(0,3)], json[randomNum(0,3)], json[randomNum(0,3)]];
 }
 
+
+//funções que só forem necessarias pro funcionamento de funções de dentro do modulo, tipo essa aqui,
+//tbm podem ficar acessiveis só dentro do modulo sem exportar!
 function randomNum(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
