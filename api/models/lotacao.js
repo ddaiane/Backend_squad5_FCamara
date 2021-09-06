9bbeb51947809b1ed48721e59b757e7bf20630b1
Debora Brum <debrumneves@gmail.com>
2021-09-06 20:04:26 -0300

    criacao models

---
+++ api/models/lotacao.js
@@ -0,0 +1,60 @@
+const { connect } = require("../DB/db.js");
+
+async function consultaCapacidadeEscritorio(req, res) {
+  try {
+    const { id_escritorio } = req.body;
+
+    const db = await connect();
+    const capacidadeLocal = await db.query(
+      `SELECT capacidade, porcentagem_de_ocupacao FROM lotacao WHERE id_escritorio = ${id_escritorio}`
+    );
+
+    // res.json({
+    //   capacidade:
+    //     capacidadeLocal.rows[0].capacidade *
+    //     capacidadeLocal.rows[0].porcentagem_de_ocupacao,
+    // });
+
+    res.json(capacidadeLocal.rows);
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+}
+
+async function alterarCapacidadeEscritorio(req, res) {
+  try {
+    const {
+      id_escritorio,
+      id_usuario,
+      capacidade: novaCapacidade,
+      porcentagem_de_ocupacao: novaPorcentagem,
+    } = req.body;
+
+    const db = await connect();
+    const conferenciaAdmin = await db.query(
+      `SELECT isAdmin FROM usuarios WHERE id_usuario = ${id_usuario}`
+    );
+
+    if (!conferenciaAdmin.rows[0].isAdmin) {
+      return res.json({ message: "Acesso negado" });
+    }
+
+    if (novaCapacidade) {
+      await db.query(
+        `UPDATE lotacao SET capacidade = ${novaCapacidade} WHERE id_escritorio = ${id_escritorio}`
+      );
+    }
+    if (novaPorcentagem) {
+      await db.query(
+        `UPDATE lotacao SET porcentagem_de_ocupacao = ${novaPorcentagem} WHERE id_escritorio = ${id_escritorio}`
+      );
+    }
+
+    res.status(200).json({message: "Alterado com sucesso"});
+
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+}
+
+module.exports = {consultaCapacidadeEscritorio, alterarCapacidadeEscritorio};
--- api/models/usuarios.js
+++ api/models/usuarios.js
@@ -1,21 +1,21 @@
 const { connect } = require("../DB/db.js");
 
 
-//retorna todos usuarios porem sem o campo senha
 async function selectUsuarios(req, res) {
   const db = await connect();
   const usuarios = await db.query("SELECT id, nome, email, isadmin FROM usuario");
   res.json(usuarios.rows);
 }
 
-// //retorna um usuario (sem o campo senha)
-// async function selectUmUsuario(req, res) {
-//   const db = await connect();
-//   const usuario = await db.query("SELECT id, nome, email, isadmin FROM USUARIO WHERE id=?", id);
-//   res.json(usuario.rows);
-// }
+
+async function selectUmUsuario(req, res) {
+  const {id_usuario} = req.body;
+  const db = await connect();
+  const usuario = await db.query(`SELECT id, nome, email, isadmin FROM USUARIO WHERE id=${id_usuario}`);
+  res.json(usuario.rows);
+}
 
 
 
 
-module.exports = { selectUsuarios };
+module.exports = { selectUsuarios, selectUmUsuario };
