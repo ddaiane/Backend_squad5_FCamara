3c2c13623a8a57fbff40a50f6784dbfb79c6bf1b
Debora Brum <debrumneves@gmail.com>
2021-09-08 10:19:57 -0300

    fix: alteração rota e controlador calendario

--- api/models/calendario.js
+++ api/models/calendario.js
@@ -1,9 +1,12 @@
-const { connect } = require("../DB/db.js");
+const { QueryTypes } = require("sequelize");
+const db = require("../DB/db");
 
 //retorna todos agendamentos FUTUROS de um determinado mes/ano em um determinado escritorio
+//localhost:3000/api/calendario/:id_escritorio?mes=9&ano=2021
 async function listarTodosAgendamentos(req, res) {
   try {
-    const { id_escritorio, mes, ano } = req.params;
+    const { id_escritorio } = req.params;
+    const { mes, ano } = req.query;
 
     //Verificação se todos os campos estão presentes, mensagem para o front
     if (!id_escritorio || !mes || !ano) {
@@ -13,22 +16,22 @@ async function listarTodosAgendamentos(req, res) {
     }
 
     const tabelaParaConsulta =
-      id_escritorio === 1 ? "agendasp" : "agendasantos";
+      id_escritorio === '1' ? "agendasp" : "agendasantos";
 
-    const db = await connect();
     const reservas = await db.query(
       `SELECT * FROM ${tabelaParaConsulta} WHERE
       extract(month from data) = ${mes} AND
       extract (year from data) = ${ano} AND
       data >= now()
-      ORDER BY data`
+      ORDER BY data`,
+      { type: QueryTypes.SELECT }
     );
 
-    if (!reservas.rows[0]) {
+    if (!reservas[0]) {
       return res.status(404).json({ reservas: false });
     }
 
-    res.json(reservas.rows);
+    res.json(reservas);
   } catch (err) {
     res.json({ error: true, message: err.message });
   }
--- api/routes/calendario.routes.js
+++ api/routes/calendario.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {listarTodosAgendamentos} = require("../models/calendario.js");
 
-router.get("/:id_escritorio&:mes&:ano", listarTodosAgendamentos);
+router.get("/:id_escritorio", listarTodosAgendamentos);
 
 module.exports = router;
