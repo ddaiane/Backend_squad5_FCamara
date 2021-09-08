542a63d4a6aa15fee92e1b1735d334e9f4233385
Debora Brum <debrumneves@gmail.com>
2021-09-08 11:20:12 -0300

    identação

--- api/models/agendamento.js
+++ api/models/agendamento.js
@@ -1,34 +1,28 @@
-const {
-  QueryTypes
-} = require('sequelize');
+const { QueryTypes } = require("sequelize");
 const db = require("../DB/db");
 
 //localhost:3000/api/agendamentos
 //parametros no body. cria retorna a linha criada no banco. funcionando
 async function criarAgendamento(req, res) {
   try {
-    const {
-      id_escritorio,
-      id_usuario,
-      data
-    } = req.body;
+    const { id_escritorio, id_usuario, data } = req.body;
 
     //Verificação se todos os campos estão presentes, mensagem para o front
     if (!id_escritorio || !id_usuario || !data) {
       return res.status(400).json({
-        message: "Todos os campos são obrigatórios"
+        message: "Todos os campos são obrigatórios",
       });
     }
 
     //Verificação se id_escritorio é valido
-    if (typeof id_escritorio != 'number' || typeof id_usuario != 'number') {
+    if (typeof id_escritorio != "number" || typeof id_usuario != "number") {
       return res.status(400).json({
-        message: "id deve ser number"
+        message: "id deve ser number",
       });
     }
     if (id_escritorio < 1 || id_escritorio > 2) {
       return res.status(400).json({
-        message: "id_escritorio invalido"
+        message: "id_escritorio invalido",
       });
     }
 
@@ -44,39 +38,35 @@ async function criarAgendamento(req, res) {
   } catch (err) {
     res.json({
       error: true,
-      message: err.message
+      message: err.message,
     });
   }
 }
-
 
 //localhost:3000/api/agendamentos/
 //parametros no body. deleta agendamento e retorna a data do agendamento cancelado. funcionando
 async function excluirAgendamento(req, res) {
   try {
-    const {
-      id_agendamento,
-      id_escritorio
-    } = req.body;
+    const { id_agendamento, id_escritorio } = req.body;
 
     //Verificação se todos os campos estão presentes, mensagem para o front
     if (!id_escritorio || !id_agendamento) {
       return res.status(400).json({
-        message: "Todos os campos são obrigatórios"
+        message: "Todos os campos são obrigatórios",
       });
     }
 
     //Verificação se parametros enviados sao numeros
-    if (typeof id_escritorio != 'number') {
+    if (typeof id_escritorio != "number") {
       return res.status(400).json({
-        message: "id deve ser number"
+        message: "id deve ser number",
       });
     }
 
     //verifica se id_escritorio é de um escritorio valido
     if (id_escritorio < 1 || id_escritorio > 2) {
       return res.status(400).json({
-        message: "id_escritorio invalido"
+        message: "id_escritorio invalido",
       });
     }
 
@@ -89,45 +79,38 @@ async function excluirAgendamento(req, res) {
     );
 
     res.json(resultado);
-    
   } catch (err) {
     res.json({
       error: true,
-      message: err.message
+      message: err.message,
     });
   }
 }
-
-
 
 //altera data. dados no body da request. retorna nova data salva. funcionando
 //localhost:3000/api/agendamentos/
 async function alterarAgendamento(req, res) {
   try {
-    const {
-      id_agendamento,
-      id_escritorio,
-      data: novaData
-    } = req.body;
+    const { id_agendamento, id_escritorio, data: novaData } = req.body;
 
     //Verificação se todos os campos estão presentes, mensagem para o front
     if (!id_escritorio || !id_agendamento || !novaData) {
       return res.status(400).json({
-        message: "Todos os campos são obrigatórios"
+        message: "Todos os campos são obrigatórios",
       });
     }
 
     //Verificação se parametros enviados sao numeros
-    if (typeof id_escritorio != 'number') {
+    if (typeof id_escritorio != "number") {
       return res.status(400).json({
-        message: "id deve ser number"
+        message: "id deve ser number",
       });
     }
 
     //verifica se id_escritorio é de um escritorio valido
     if (id_escritorio < 1 || id_escritorio > 2) {
       return res.status(400).json({
-        message: "id_escritorio invalido"
+        message: "id_escritorio invalido",
       });
     }
 
@@ -135,34 +118,30 @@ async function alterarAgendamento(req, res) {
       id_escritorio === 1 ? "agendasp" : "agendasantos";
 
     const resultado = await db.query(
-      `UPDATE ${tabelaParaConsulta} SET data = '${novaData}' WHERE id_agendamento = ${id_agendamento} RETURNING data`, {
-        type: QueryTypes.UPDATE
+      `UPDATE ${tabelaParaConsulta} SET data = '${novaData}' WHERE id_agendamento = ${id_agendamento} RETURNING data`,
+      {
+        type: QueryTypes.UPDATE,
       }
     );
 
-    res.status(200).json(
-      resultado[0]
-    );
+    res.status(200).json(resultado[0]);
   } catch (err) {
     res.json({
       error: true,
-      message: err.message
+      message: err.message,
     });
   }
 }
-
 
 //retorna todos os agendamentos FUTUROS do usuario em ordem de data
 //localhost:3000/api/agendamentos/:id_usuario funcionando
 async function listarAgendamentos(req, res) {
   try {
-    const {
-      id_usuario
-    } = req.params;
+    const { id_usuario } = req.params;
     //Verificação se parametro enviado é um numero
-    if (typeof id_usuario != 'number') {
+    if (typeof id_usuario != "number") {
       return res.status(400).json({
-        message: "id invalido"
+        message: "id invalido",
       });
     }
 
@@ -173,20 +152,17 @@ async function listarAgendamentos(req, res) {
     ORDER BY data`;
 
     const resultado = await db.query(query, {
-      type: QueryTypes.SELECT
+      type: QueryTypes.SELECT,
     });
 
     res.status(200).json(resultado);
   } catch (err) {
     res.json({
       error: true,
-      message: err.message
+      message: err.message,
     });
   }
 }
-
-
-
 
 module.exports = {
   criarAgendamento,
