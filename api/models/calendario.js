20ffffdaae05ee9001446a15c314fcc4b4b29b56
Debora Brum <debrumneves@gmail.com>
2021-09-11 10:40:11 -0300

    nova rota: Listar vagas por dia

--- api/models/calendario.js
+++ api/models/calendario.js
@@ -7,7 +7,6 @@ const { verificaEscritorio } = require("./uteis");
 async function listarTodosAgendamentos(req, res) {
   try {
     const { id_escritorio, mes, ano } = req.params;
-    
 
     //Verificação se todos os campos estão presentes, mensagem para o front
     if (!id_escritorio || !mes || !ano) {
@@ -16,14 +15,14 @@ async function listarTodosAgendamentos(req, res) {
         .json({ message: "Todos os campos são obrigatórios" });
     }
 
-    if (!verificaEscritorio(id_escritorio)){
+    if (!verificaEscritorio(id_escritorio)) {
       return res.status(400).json({
         message: "id escritorio invalido",
       });
     }
 
     const tabelaParaConsulta =
-      id_escritorio === '1' ? "agendasp" : "agendasantos";
+      id_escritorio === "1" ? "agendasp" : "agendasantos";
 
     const reservas = await db.query(
       `SELECT * FROM ${tabelaParaConsulta} WHERE
@@ -44,5 +43,49 @@ async function listarTodosAgendamentos(req, res) {
   }
 }
 
+async function listarVagasPorDia(req, res) {
+  console.log("entra?");
+  //Pegar qual o escritório OK
+  //Pegar a capcidade do escritório OK
+  //Pegar os agendamentos do escritório nesse dia OK
+  //Fazer o JOIN para pegar a interseção entre capacidade e agendamento
+  try {
+    const { id_escritorio } = req.params;
+    const { data } = req.body;
 
-module.exports = { listarTodosAgendamentos };
+    //Verificação se todos os campos estão presentes, mensagem para o front
+    if (!id_escritorio || !data) {
+      return res
+        .status(400)
+        .json({ message: "Todos os campos são obrigatórios" });
+    }
+
+    if (!verificaEscritorio(id_escritorio)) {
+      return res.status(400).json({
+        message: "id escritorio invalido",
+      });
+    }
+
+    const capacidade = await db.query(
+      `SELECT capacidade, vagas FROM lotacao 
+      WHERE id_escritorio = ${id_escritorio}`,
+      { type: QueryTypes.SELECT }
+    );
+
+    const tabelaParaConsulta =
+      id_escritorio === "1" ? "agendasp" : "agendasantos";
+
+    const agendamentos = await db.query(
+      `SELECT count(*) FROM ${tabelaParaConsulta} WHERE data = '${data}' GROUP BY data`,
+      { type: QueryTypes.SELECT }
+    );
+
+    const vagas = capacidade[0].vagas - Number(agendamentos[0].count);
+
+    res.status(200).json({vagas: vagas});
+  } catch (err) {
+    res.status(404).json({ error: true, message: err.message });
+  }
+}
+
+module.exports = { listarTodosAgendamentos, listarVagasPorDia };
